9d35a54cd9b94764b617c1ae8d3f955a36aec99e
Johannes Wilde <git@jowil.de>
2025-11-02 09:02:27 +0100

    As noted in the stackoverflow link, the method should not require std::vector here.
    https://stackoverflow.com/questions/2114358/using-stl-allocator-with-stl-vectors

--- main.cpp
+++ main.cpp
@@ -1,107 +1,19 @@
 #include <cstdlib>
 #include <iostream>
+#include <span>
 #include <vector>
 
-template<class T>
-struct ConstrainedAllocator
-{
-    typedef T value_type;
 
-    ConstrainedAllocator(T * const address, size_t const count)
-        : address_(address)
-        , count_(count)
-    {
-        // intentionally empty
-    }
-
-    ConstrainedAllocator(ConstrainedAllocator const &) = default;
-    ConstrainedAllocator(ConstrainedAllocator &&) = default;
-
-    // todo: understand what this is needed for!
-    template<class U>
-    constexpr ConstrainedAllocator(ConstrainedAllocator<U> const & other) noexcept
-        : address_((T *)(other.address_))
-        , count_(other.count_ * sizeof(U) / sizeof(T))
-    {
-        // intentionally empty
-    }
-
-    size_t max_size() const
-    {
-        return count_;
-    }
-
-    [[nodiscard]] T * allocate(std::size_t n)
-    {
-        if (max_size() < n)
-        {
-            throw std::bad_array_new_length();
-        }
-
-        if (nullptr == address_)
-        {
-            throw std::bad_alloc();
-        }
-
-        return address_;
-    }
-
-    void deallocate(T* p, std::size_t n) noexcept
-    {
-        // intentionally empty
-    }
-
-    T * const address_;
-    size_t const count_;
-};
-
-template<class T, class U>
-bool operator==(ConstrainedAllocator<T> const &, ConstrainedAllocator<U> const &) { return true; }
-
-template<class T, class U>
-bool operator!=(ConstrainedAllocator<T> const &, ConstrainedAllocator<U> const &) { return false; }
-
-
-
-
-void method(std::vector<int> const & bla)
+void method(std::span<int, std::dynamic_extent> const span)
 {
     std::cout << "bla: " <<
-        bla.size()
+        span.size()
               << std::endl;
 }
 
 int main()
 {
     std::vector<int> owningVector(/*n*/ 100, /*val*/ 0);
-    method(owningVector);
-
-    {
-        int * const pointer = owningVector.data();
-        size_t const count = owningVector.size();
-
-        ConstrainedAllocator<int> const allocator{pointer, count};
-
-        std::vector<int, ConstrainedAllocator<int>> v{count, allocator};
-        v[0] = 1;
-
-        v.resize(5);
-        try
-        {
-            v.resize(count + 1);
-        }
-        catch (std::exception const & e)
-        {
-            std::cout << "resize(101): " << e.what() << std::endl;
-        }
-
-        // https://stackoverflow.com/questions/2114358/using-stl-allocator-with-stl-vectors
-        method(v); // Incorrect type due to different allocator in std::vector<T, Alloc_>.
-        method(*((std::vector<int> const *)(&v))); // Does not work!
-        method(*((std::vector<int> const *)(((std::uint8_t)&v) + sizeof(ConstrainedAllocator<int>)))); // Does not work!
-    }
-
-    std::cout << "v[0] = " << owningVector[0] << std::endl;
-
+    method(std::span<int, std::dynamic_extent>(owningVector));
     return 0;
 }
